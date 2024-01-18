952c1aa70c7d43effda59f568d0f94efae2974a6
Eisyger <140314733+Eisyger@users.noreply.github.com>
2024-01-18 15:58:27 +0100

    str output accountdata

--- src/data.py
+++ src/data.py
@@ -4,4 +4,14 @@ class AccountData:
         self.loginName = ""
         self.LoginEmail = ""
         self.website = ""
-        self.passwort = ""
+        self.password = ""
+        self.note = ""
+
+    def __str__(self) -> str:
+        return (
+            f"Account Name   : {self.nameAccount}\n"
+            f"Website        : {self.website}\n"
+            f"Login Name     : {self.loginName}\n"
+            f"Login Email    : {self.LoginEmail}\n"            
+            f"Notizen        : {self.note}"
+        )
