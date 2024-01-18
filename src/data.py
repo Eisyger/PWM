a48abd4699efe3625366d05fb62995d61af96a4d
Eisyger <140314733+Eisyger@users.noreply.github.com>
2024-01-18 15:52:01 +0100

    impl accountdata

---
+++ src/data.py
@@ -0,0 +1,7 @@
+class AccountData:
+    def __init__(self):
+        self.nameAccount = ""
+        self.loginName = ""
+        self.LoginEmail = ""
+        self.website = ""
+        self.passwort = ""
