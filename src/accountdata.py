ad8284cd911d777f1e1bcb7b839723f90a4e3387
Eisyger <140314733+Eisyger@users.noreply.github.com>
2024-02-28 22:04:11 +0100

    remove save from accountdata, save.py is for saving

--- src/accountdata.py
+++ src/accountdata.py
@@ -22,10 +22,10 @@ class AccountData:
     def __str__(self) -> str:
         return "\n".join(f""
                          f"{key.ljust(12)}:\t{value}"
-                         for key, value in self._get_dict().items()
+                         for key, value in self.get_dict().items()
                          if key != "PASSWORD") + "\n"
 
-    def _get_dict(self) -> dict:
+    def get_dict(self) -> dict:
         return {
             "ACCOUNT_NAME": self.account_name,
             "LOGIN_NAME": self.login_name,
@@ -42,23 +42,6 @@ class AccountData:
                            data["WEBSITE"],
                            data["PASSWORD"])
 
-    def save(self, path: str, auth_key: bytes):
-        Save.save_file(path, [self._get_dict()], auth_key)
 
 
-"""
-d1 = AccountData("Facebook", "Marie", "asdf@gmx.de", "facebook.de", "123")
-d2 = AccountData("Google", "Marie", "asdf@gmx.de111111", "google.de", "1234")
-
-
-seed = "DeinEigenerFaktor"
-
-# Erzeuge einen Schlüssel durch Hashing der Seed-Zeichenkette
-key_bytes = hashlib.sha256(seed.encode('utf-8')).digest()
-
-# Konvertiere den Schlüssel in base64
-base64_key = base64.urlsafe_b64encode(key_bytes)
-
-d1.save("save_file_data.mpw", base64_key)
-print(json.loads(Load.load_file("save_file_data.mpw", base64_key)))"""
 
