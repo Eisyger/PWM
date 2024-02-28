02eba94fcba04d030b0913a370b8b459760f5635
Eisyger <140314733+Eisyger@users.noreply.github.com>
2024-02-28 22:07:15 +0100

    implement print, get_password, remove and save

--- src/account_manager.py
+++ src/account_manager.py
@@ -1,64 +1,82 @@
 import json
 import os
+import clipboard
 from load import Load
+from save import Save
 from accountdata import AccountData
 
 
 class AccountManager:
     def __init__(self, path_data: str, auth_key: bytes):
-        self.accounts = []
-        self.auth_key = auth_key
-        self.path_data = path_data
+        self._accounts = []
+        self._auth_key = auth_key
+        self._path_data = path_data
 
         # load data from file and generate list
         if os.path.exists(path_data):
-            self.accounts = self.__load_data_from_file()
-            print("-" * 50)
+            self._accounts = self.__load_data_from_file()
             print("Daten geladen.")
-            print("-" * 50)
         else:
             print("#" * 50)
             print("Keine Daten gefunden.")
             print("#" * 50)
 
     def __load_data_from_file(self):
-        data_from_file = Load.load_file(self.path_data, self.auth_key)
+        data_from_file = Load.load_file(self._path_data, self._auth_key)
         if data_from_file:
             json_from_file = json.loads(data_from_file)
             return [AccountData.creat_acc_from_dict(account) for account in json_from_file]
 
-    def __print_all(self, short=True):
-        index = 1
+    def print_data(self, name="", all=False, short=True, ):
+        i = 0
         print("Auflistung aller Accounts:")
-        for acc in self.accounts:
-            print("-" * 50)
-            if short:
-                print(acc.account_name)
-            else:
-                print(acc)
-            index += 1
         print("-" * 50)
-        print(f"Es wurden die Daten von {index} Accounts ausgegeben.")
 
-    def run(self):
-        pass
+        for acc in self._accounts:
+            i += 1
+
+            if acc.account_name == name and not all:
+                print(f"{i}:\n{acc}")
+
+            if all:
+                if short:
+                    print(f"{i}: {acc.account_name}")
+                else:
+                    print(f"{i}:\n{acc}")
+
+        print("-" * 50)
+
+    def get_password(self, name):
+        for acc in self._accounts:
+            if acc.account_name == name:
+                clipboard.copy_to_clipboard(acc.password)
+                print("Passwort in Zwischenablage gespeichert.")
+
+    def remove(self, name):
+        for i, acc in enumerate(self._accounts):
+            if acc.account_name == name:
+                self._accounts.pop(i)
+                print(f"Der Account {name} wurde gelöscht.")
+
+    def save(self):
+        Save.save_file(self._path_data, [acc.get_dict() for acc in self._accounts], self._auth_key)
 
     def create_test_data(self):
-        print("Nock keine Accounts vorhanden. Willst du einen neuen Account anlegen?")
+        """Generiert eine Testdatenbank. Führe dazu test_account_manager.py aus!"""
+
+        print("Noch keine Accounts vorhanden. Willst du einen neuen Account anlegen?")
         print("Anlegen von Accounts.")
-        d1 = AccountData("Facebook", "Marie", "asdf@gmx.de", "facebook.de", "123")
-        d2 = AccountData("Instagram", "Marie", "asdf@gmx.de", "instagram.de", "123")
-        d3 = AccountData("gdasf", "Marie", "asdf@gmx.de", "instagram.de", "123")
-        d4 = AccountData("asdf", "Marie", "asdf@gmx.de", "instagram.de", "123")
+        d1 = AccountData("Facebook", "Marie", "asdf@gmx.de", "facebook.de", "sarhaee")
+        d2 = AccountData("Instagram", "Marie", "asdf@gmx.de", "instagram.de", "128791aer49g3")
+        d3 = AccountData("gdasf", "Marie", "asdf@gmx.de", "instagram.de", "c1e501123gv ")
+        d4 = AccountData("asdf", "Marie", "asdf@gmx.de", "instagram.de", "sdsadg79)8/")
         print("Speichere Accounts.")
-        d1.save(self.path_data, self.auth_key)
-        d2.save(self.path_data, self.auth_key)
-        d3.save(self.path_data, self.auth_key)
-        d4.save(self.path_data, self.auth_key)
+        accounts = [d1, d2, d3, d4]
+        Save.save_file(self._path_data, [acc.get_dict() for acc in accounts], self._auth_key)
 
         print("Lade Accounts.")
-        self.accounts = self.__load_data_from_file()
+        self._accounts = self.__load_data_from_file()
         print("Geladen!")
 
-        for acc in self.accounts:
+        for acc in self._accounts:
             print(str(acc))
