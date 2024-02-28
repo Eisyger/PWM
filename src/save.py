e3cfb85b023bd1e875175280631f5e7cbd03a4de
Eisyger <140314733+Eisyger@users.noreply.github.com>
2024-02-28 22:04:59 +0100

    no update needed, full data is loaded, edited will be saved

--- src/save.py
+++ src/save.py
@@ -22,9 +22,6 @@ class Save:
 
     @staticmethod
     def save_file(path: str, account_data: list, auth: bytes = b""):
-        # update existing data from file - add new data if possible
-        # update parameter account_data as reference
-        account_data = Save._update_data(path, account_data, auth)
 
         # creat a json from the data
         json_data = json.dumps(account_data, indent=4).encode('utf-8')
@@ -46,19 +43,4 @@ class Save:
         cipher = Fernet(auth)
         return cipher.encrypt(data)
 
-    @staticmethod
-    def _update_data(path: str, account_data: list, auth: bytes):
-        # load the current data from file, and converts it back to a list via json.loads
-        loaded_data = Load.load_file(path, auth)
-        if loaded_data:
-            loaded_data = json.loads(loaded_data)
-        else:
-            # return the new data, when no data at all is in the file
-            return account_data
 
-        # check if data is the same.
-        # if not append the account_data on the loaded data and return the loaded data
-        for data in account_data:
-            if data not in loaded_data:
-                loaded_data.append(data)
-        return loaded_data
